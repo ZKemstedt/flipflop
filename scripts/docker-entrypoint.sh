0c88d76da311ac77370e9782efa93e57457fa8f6
Zephyro <zkemstedt@gmail.com>
2021-09-09 13:30:56 +0200

    removed unused old eula fixer

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,20 +3,5 @@ mv /tmp/build/Dynmap.jar /spigot/plugins/
 ssh-keygen -A
 
 /scripts/start.sh
-# FUCK YOU
-# if [ ! -z $DEV ]
-# then
-#     # generate fresh rsa, dsa keys
-#     if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-#         ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-#     fi
-#     if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-#         ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-#     fi
-# fi
 
-# # prepare run dir
-# if [ ! -d "/var/run/sshd" ]; then
-#     mkdir -p /var/run/sshd
-# fi
 exec "$@"
