488955b4fd78b5d8f18ed9c7d7edc5407af0388f
Zephyro Kemstedt <zkemstedt@gmail.com>
2021-09-14 14:06:32 +0200

    fix setup.sh moving over conf files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,7 @@
 # On first start after build
 if [ ! "$(ls -A /data)" ]; then
     # Move over configuration files
-    mv /tmp/conf/* /data/
-    rm -rf /tmp/conf
+    mkdir /data/plugins
 
     # Add functions from utils.sh
     . ${SCRIPTS:-/}utils.sh
@@ -16,9 +15,11 @@ if [ ! "$(ls -A /data)" ]; then
     curl -L -o /data/spigot.jar https://download.getbukkit.org/spigot/spigot-1.17.1.jar
 
     # Download Dynmap
+    mkdir -p /data/plugins/dynmap
     curl -L -o /data/plugins/Dynmap.jar https://dev.bukkit.org/projects/dynmap/files/3435158/download
 
     # Download CoreProtect
+    mkdir -p /data/plugins/CoreProtect
     curl -L -o /data/plugins/CoreProtect.jar https://github.com/PlayPro/CoreProtect/releases/download/v20.1/CoreProtect-20.1.jar
 
     # Download SQL connector
@@ -28,6 +29,21 @@ if [ ! "$(ls -A /data)" ]; then
     # Set up rcon
     curl -L -o /home/pingu/rcon.tar.gz https://github.com/itzg/rcon-cli/releases/download/1.4.8/rcon-cli_1.4.8_linux_amd64.tar.gz
     tar -xf /home/pingu/rcon.tar.gz -C /home/pingu/
+fi
+
+# On the first entry after building image, add (overwrite) fresh configuration files
+if [ -d /tmp/conf ]; then
+    # plugin CoreProtect 
+    mv plugins/CoreProtect/config.yml /data/plugins/CoreProtect/
+    # plugin Dynmap
+    mv plugins/dynmap/configuration.txt /data/plugins/dynmap/configuration.txt
+    # Core (vanilla / spigot / bukkit)
+    mv /tmp/conf/*.yml /data/
+    mv /tmp/conf/*.json /data/
+    mv /tmp/conf/server.properties /data/
+    mv /tmp/conf/server-icon /data/
+
+    rm -rf /tmp/conf
 fi
 
 # start ssh server
